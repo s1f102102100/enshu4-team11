aa4c254fe5039b55b0a6084499504111b9d88f08
s1f102102100 <s1f102102100@iniad.org>
2023-01-09 01:40:27 +0900

    諸々消去、移動とか

--- authtest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('priv', views.private_page, name='priv'),
-    path('pub', views.public_page, name='pub'),
-    
-]
